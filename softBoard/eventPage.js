8718769648737567c37091568e68c26682956f84
DeveshChau <deveshachaudhari@gmail.com>
2020-07-26 17:07:21 +0530

    pin unique url

--- softBoard/eventPage.js
+++ softBoard/eventPage.js
@@ -20,9 +20,14 @@ function setBadge(badgeTotle) {
     chrome.browserAction.setBadgeText({ "text": badgeTotle.toString() });
 }
 
+function urlNotPresent(pins, tabURL) {
+    return !pins.urlArray.find(obj => obj.url === tabURL )
+}
+
 setUrlTitle({ 'total': 120, 'urlArray': [] });
 setBadge(120);
 chrome.browserAction.onClicked.addListener(function () {
+
     let tabTitle = '';
     let tabURL = '';
     chrome.tabs.query({
@@ -32,10 +37,10 @@ chrome.browserAction.onClicked.addListener(function () {
         tabTitle = tabs[0].title;
         tabURL = tabs[0].url;
     });
-    chrome.storage.sync.get(['total', 'urlArray'], function (pins) {        
+    chrome.storage.sync.get(['total', 'urlArray'], function (pins) {
         if (getObjectLength(pins)) {
-            console.log('limit end');            
-        } else {
+            console.log('limit end');
+        } else if (urlNotPresent(pins, tabURL)) {
             pins.total = pins.total - 1;
             pins.urlArray.push({ 'title': tabTitle, 'url': tabURL });
             setUrlTitle(pins);
